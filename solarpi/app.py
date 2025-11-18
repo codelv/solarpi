6165d40edcce9b9cbf00875569be9ad8ee58cbc3
frmdstryr <frmdstryr@protonmail.com>
2025-11-18 16:45:00 -0500

    Updates

--- solarpi/app.py
+++ solarpi/app.py
@@ -6,7 +6,7 @@ import logging
 import json
 import aiosqlite
 import dataclasses
-from datetime import datetime, time
+from datetime import datetime, date, time, timedelta
 from typing import ClassVar, Optional
 from logging.handlers import RotatingFileHandler
 
@@ -21,65 +21,333 @@ env = Environment(
 )
 log = logging.getLogger("solarpi")
 
+
 routes = web.RouteTableDef()
 routes.static('/static', os.path.join(os.path.dirname(__file__), 'static'))
 
+
+async def load_energy_chart(d: date):
+    dates = []
+    last_state: Optional[State] = None
+    solar_energy_output = []
+    battery_discharge_energy = []
+    battery_charge_energy = []
+    inverter_energy = []
+    start = datetime.combine(d, time(23, 59, 59))
+    for i in range(8, -1, -1):
+        d = start - timedelta(days=i)
+        # Exclude empty readings
+        async with DB.execute((
+            "SELECT * FROM solar "
+            "WHERE timestamp <= ? AND charger_total_energy > 0 AND battery_total_charge_energy > 0 AND battery_total_discharge_energy > 0 "
+            "ORDER BY timestamp DESC LIMIT 1"
+        ), (d.timestamp(),)) as cursor:
+            state: Optional[State] = None
+            async for row in cursor:
+                state = State(*row)
+                log.warning(f"Date {d} {state}")
+                if last_state:
+                    solar_energy_output.append(max(0, state.charger_total_energy - last_state.charger_total_energy))
+                    battery_discharge_energy.append(max(0, state.battery_total_discharge_energy - last_state.battery_total_discharge_energy))
+                    battery_charge_energy.append(max(0, state.battery_total_charge_energy - last_state.battery_total_charge_energy))
+                    inverter_energy.append(solar_energy_output[-1]-battery_charge_energy[-1]+battery_discharge_energy[-1])
+                    dates.append(d.date())
+                last_state = state
+
+    energy_chart_data = {
+        "labels": [str(d) for d in dates],
+        "datasets": [
+            {
+                "label": "Solar energy output (kWh)",
+                "data": solar_energy_output,
+                "borderWidth": 1,
+            },
+            {
+                "label": "Battery discharge energy (kWh)",
+                "data": battery_discharge_energy,
+                "borderWidth": 1,
+            },
+            {
+                "label": "Battery charge energy (kWh)",
+                "data": battery_charge_energy,
+                "borderWidth": 1,
+            },
+            {
+                "label": "Inverter/Load energy (kWh)",
+                "data": inverter_energy,
+                "borderWidth": 1,
+            },
+        ]
+    }
+    log.warning(energy_chart_data)
+    return {
+        "type": 'bar',
+        "data": energy_chart_data,
+        "options": {
+             "scales": {
+                 "y": {
+                     "beginAtZero": True,
+                 }
+            },
+        }
+    }
+
+@routes.get("/api/sidebar/")
+async def sidebar(request: web.Request):
+    template = env.get_template("sidebar.html")
+    async with DB.execute("SELECT * FROM solar ORDER BY timestamp DESC LIMIT 1") as cursor:
+        async for row in cursor:
+            state = State(*row)
+    content = template.render(state=state or State())
+    return web.Response(text=content, content_type="text/html")
+
+
 @routes.get("/")
-async def index(request):
+async def index(request: web.Request):
     template = env.get_template("index.html")
 
-    d = datetime.now().date()
-    start_of_day = datetime.combine(d, time(0, 0))
+    try:
+        if date_str := request.query.get("d", ''):
+            d = datetime.strptime(date_str, "%Y-%m-%d").date()
+        else:
+            d = datetime.now().date()
+    except Exception as e:
+        log.warning(f"Invalid date query: {e}")
+        d = datetime.now().date()
+
+    start_of_day = datetime.combine(d, time(0, 0)).timestamp()
+    end_of_day = datetime.combine(d, time(23, 59, 59)).timestamp()
     timestamps = []
-    battery_voltages = []
-    chart_data = {
+    battery_voltage = []
+    battery_current = []
+    battery_power = []
+    charger_current = []
+    charger_voltage = []
+    charger_power = []
+    inverter_current = []
+    solar_voltage = []
+    solar_current = []
+    inverter_power = []
+    battery_soc = []
+    charger_temp = []
+    battery_temp = []
+    room_temp = []
+    voltages_chart_data = {
         "labels": timestamps,
         "datasets": [
             {
-                "label": "Battery Voltage",
-                "data": battery_voltages,
+                "label": "Battery Voltage (V)",
+                "data": battery_voltage,
                 "pointRadius": 0,
-                #"spanGaps": True
+            },
+            {
+                "label": "Battery Current (A)",
+                "data": battery_current,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Solar Voltage (V)",
+                "data": solar_voltage,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Solar Current (A)",
+                "data": solar_current,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Charger Voltage (V)",
+                "data": charger_voltage,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Charger Current (A)",
+                "data": charger_current,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Inverter Current (A)",
+                "data": inverter_current,
+                "pointRadius": 0,
+            },
+        ]
+    }
+
+    power_chart_data = {
+        "labels": timestamps,
+        "datasets": [
+            {
+                "label": "Battery Power (W)",
+                "data": battery_power,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Charger Power (W)",
+                "data": charger_power,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Inverter Power (W)",
+                "data": inverter_power,
+                "pointRadius": 0,
             }
         ]
     }
+
+    soc_chart_data = {
+        "labels": timestamps,
+        "datasets": [
+            {
+                "label": "Battery State of Charge (Ah)",
+                "data": battery_soc,
+                "pointRadius": 0,
+            },
+        ]
+    }
+
+    temp_chart_data = {
+        "labels": timestamps,
+        "datasets": [
+            {
+                "label": "Charger Temp (°C)",
+                "data": charger_temp,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Battery Temp (°C)",
+                "data": battery_temp,
+                "pointRadius": 0,
+            },
+            {
+                "label": "Room Temp (°C)",
+                "data": room_temp,
+                "pointRadius": 0,
+            },
+        ]
+    }
+
     #data = []
-    current = None
-    async with DB.execute("SELECT * FROM solar WHERE timestamp >= ? ORDER BY timestamp DESC", (start_of_day.timestamp(), )) as cursor:
+    state: Optional[State] = None
+    async with DB.execute("SELECT * FROM solar WHERE timestamp >= ? AND timestamp <= ? ORDER BY timestamp ASC", (start_of_day, end_of_day)) as cursor:
         async for row in cursor:
             state = State(*row)
-            if current is None:
-                current = state
-            timestamps.append(state.timestamp)
-            battery_voltages.append(state.battery_voltage)
+            timestamps.append(state.timestamp*1000)
+            battery_voltage.append(state.battery_voltage)
+            if state.battery_is_charging:
+                battery_current.append(state.battery_current)
+            else:
+                battery_current.append(-state.battery_current)
 
-            #data.append(State(*row))
+            battery_power.append(state.battery_power)
+            battery_soc.append(state.battery_ah)
+            charger_voltage.append(state.charger_voltage)
+            charger_current.append(state.charger_current)
+            charger_power.append(state.charger_power)
+            solar_voltage.append(state.solar_panel_voltage)
+            solar_current.append(state.solar_panel_current)
+            inverter_current.append(state.inverter_current)
+            inverter_power.append(state.inverter_power)
 
-    chart = {
+            charger_temp.append(state.charger_temp)
+            battery_temp.append(state.battery_temp)
+            room_temp.append(state.room_temp)
+
+
+
+    power_chart = {
         "type": 'line',
-        "data": chart_data,
+        "data": power_chart_data,
         "options": {
             "responsive": True,
-            #"normalized": True,
+            "normalized": True,
             "animation": False,
+            "spanGaps": True,
             "scales": {
                 "x": {
-                    "type": 'timeseries',
+                    "type": 'time',
                 }
             },
             "plugins": {
                 "legend": {
                     "position": 'top',
                 },
-                "title": {
-                    "display": True,
-                    "text": 'Battery Voltage'
-                }
             }
         }
     }
 
-    content = template.render(chart_data=json.dumps(chart), current=current)
+    voltages_chart = {
+        "type": 'line',
+        "data": voltages_chart_data,
+        "options": {
+            "responsive": True,
+            "normalized": True,
+            "animation": False,
+            "spanGaps": True,
+            "scales": {
+                "x": {
+                    "type": 'time',
+                }
+            },
+            "plugins": {
+                "legend": {
+                    "position": 'top',
+                },
+            }
+        }
+    }
+
+    soc_chart = {
+        "type": 'line',
+        "data": soc_chart_data,
+        "options": {
+            "responsive": True,
+            "normalized": True,
+            "animation": False,
+            "spanGaps": True,
+            "scales": {
+                "x": {
+                    "type": 'time',
+                }
+            },
+            "plugins": {
+                "legend": {
+                    "position": 'top',
+                },
+            }
+        }
+    }
+
+    temp_chart = {
+        "type": 'line',
+        "data": temp_chart_data,
+        "options": {
+            "responsive": True,
+            "normalized": True,
+            "animation": False,
+            "spanGaps": True,
+            "scales": {
+                "x": {
+                    "type": 'time',
+                }
+            },
+            "plugins": {
+                "legend": {
+                    "position": 'top',
+                },
+            }
+        }
+    }
+
+    energy_chart = await load_energy_chart(d)
+    content = template.render(
+        power_chart=json.dumps(power_chart),
+        voltages_chart=json.dumps(voltages_chart),
+        soc_chart=json.dumps(soc_chart),
+        temp_chart=json.dumps(temp_chart),
+        energy_chart=json.dumps(energy_chart),
+        selected_date=d,
+        state=state or State()
+    )
     return web.Response(text=content, content_type="text/html")
 
 
--- solarpi/db.py
+++ solarpi/db.py
@@ -14,6 +14,7 @@ class State:
     battery_is_temp_in_f: ClassVar[bool] = True
     battery_ah: float = 0
     battery_temp: float = 0
+    battery_capcity: ClassVar[float] = 600
     battery_total_charge_energy: float = 0
     battery_total_discharge_energy: float = 0
 
@@ -26,6 +27,41 @@ class State:
     room_temp: float = 0
 
     _instance: ClassVar["State"] = None
+
+    @property
+    def solar_panel_current(self):
+        if v := self.solar_panel_voltage:
+            return round(self.charger_voltage/v * self.charger_current, 2)
+        return 0
+
+    @property
+    def battery_power(self):
+        sign = 1 if self.battery_is_charging else -1
+        return round(self.battery_voltage * sign * self.battery_current, 2)
+
+    @property
+    def battery_percent(self):
+        return round(100 * self.battery_ah / self.battery_capcity, 2)
+
+    @property
+    def charger_power(self):
+        return round(self.charger_voltage * self.charger_current, 2)
+
+    @property
+    def inverter_voltage(self):
+        return max(self.charger_voltage, self.battery_voltage)
+
+    @property
+    def inverter_current(self):
+        if self.battery_is_charging:
+            # If charger outputs 14A and battery is charging at 10A, inverter is using 4A
+            return max(0, self.charger_current - self.battery_current)
+        # If charger outputs 4A and battery is discharging at 10A, inverter is using 14A
+        return self.charger_current + self.battery_current
+
+    @property
+    def inverter_power(self):
+        return round(self.inverter_voltage * self.inverter_current, 2)
 
     @classmethod
     def instance(cls):
